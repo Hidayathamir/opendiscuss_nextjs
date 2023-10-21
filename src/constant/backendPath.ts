de5599ff56264f09f8315304849a1477bbb9dc37
Hidayat Hamir <hidayat.03@erajaya.com>
2023-10-21 23:52:00 +0700

    fix: backend port use 9080, latest port backend repo

--- src/constant/backendPath.ts
+++ src/constant/backendPath.ts
@@ -1,6 +1,6 @@
-export const URL_API_REGISTER = "http://localhost:8080/api/v1/register"
-export const URL_API_LOGIN = "http://localhost:8080/api/v1/login"
-export const URL_API_QUESTIONS = "http://localhost:8080/api/v1/questions"
+export const URL_API_REGISTER = "http://localhost:9080/api/v1/register"
+export const URL_API_LOGIN = "http://localhost:9080/api/v1/login"
+export const URL_API_QUESTIONS = "http://localhost:9080/api/v1/questions"
 export enum VoteOption {
   ThumbsUp = "thumbsup",
   ThumbsDown = "thumbsdown",
@@ -9,8 +9,8 @@ export const URL_API_VOTE_QUESTION = (
   questionId: number,
   voteOption: VoteOption,
 ) => {
-  return `http://localhost:8080/api/v1/questions/${questionId}/${voteOption}`
+  return `http://localhost:9080/api/v1/questions/${questionId}/${voteOption}`
 }
 export const URL_API_DELETE_QUESTION = (questionId: number) => {
-  return `http://localhost:8080/api/v1/questions/${questionId}`
+  return `http://localhost:9080/api/v1/questions/${questionId}`
 }
